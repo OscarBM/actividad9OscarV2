c6b4ba6681f9e678f7edf8585accaf7a66186f0a
Oscar <oscarismaelbm@gmail.com>
2018-10-19 10:38:43 -0500

    Initial commit

---
+++ app/src/main/java/com/tecmi/oscar/actividad9oscar/MainActivity.java
@@ -0,0 +1,20 @@
+package com.tecmi.oscar.actividad9oscar;
+
+import android.Manifest;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("RECEIVER", "Hey");
+
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.RECEIVE_SMS}, 1);
+
+    }
+}
---
+++ app/src/main/java/com/tecmi/oscar/actividad9oscar/SmsReceiver.java
@@ -0,0 +1,41 @@
+package com.tecmi.oscar.actividad9oscar;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.util.Log;
+
+public class SmsReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d("RECEIVER", intent.getAction().toString());
+
+        if(intent.getAction().equals("android.provider.Telephony.SMS_RECEIVED")){
+            Log.d("RECEIVER", "Hola amigo");
+
+            final Bundle bundle = intent.getExtras();
+            Log.d("RECEIVER", bundle.get("pdus").toString());
+            final Object[] pdusObj = (Object[]) bundle.get("pdus");
+            for (int i = 0; i < pdusObj.length; i++){
+
+                SmsMessage currentMessage = SmsMessage.createFromPdu((byte[]) pdusObj[i]);
+                String phoneNumber = currentMessage.getDisplayOriginatingAddress();
+
+                String message = currentMessage.getMessageBody();
+
+                Log.d("RECEIVER", "numer000: "+phoneNumber+" Mensaje: "+message );
+
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com.mx/"));
+                context.startActivity(browserIntent);
+
+
+            }
+
+        }
+
+    }
+}
